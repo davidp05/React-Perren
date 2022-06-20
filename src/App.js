dc55a2a5c843e23c105e8f977064c49310b2e246
david.perren <david.perren05@gmail.com>
2022-06-20 17:30:34 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logo from './assets/logo-dev.jpg';
 import './App.css';
+import logoCarrito from './assets/carrito.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <header class="header">
+        <div class="container logo-nav-container">
+            <img src={logo} alt="logo"></img>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="#">CATEGORIA 1</a></li>
+                    <li><a href="#">CATEGORIA 2</a></li>
+                    <li><a href="#">CATEGORIA 3</a></li>
+                    <li><img id='carrito' src={logoCarrito} alt="logoCarrito"></img></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
     </div>
   );
 }
