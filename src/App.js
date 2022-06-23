c27448ebe56b37d12420ad23bf13800d9f9bf4a5
david.perren <david.perren05@gmail.com>
2022-06-22 22:59:51 -0300

    Desafio landing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './assets/logo-dev.jpg';
 import './App.css';
-import logoCarrito from './assets/carrito.png'
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">  
-      <header class="header">
-        <div class="container logo-nav-container">
-            <img src={logo} alt="logo"></img>
-            <nav class="navigation">
-                <ul>
-                    <li><a href="#">CATEGORIA 1</a></li>
-                    <li><a href="#">CATEGORIA 2</a></li>
-                    <li><a href="#">CATEGORIA 3</a></li>
-                    <li><img id='carrito' src={logoCarrito} alt="logoCarrito"></img></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+      <NavBar/>
+      <ItemListContainer greeting="Bienvenido a la tienda"/>
     </div>
   );
 }
